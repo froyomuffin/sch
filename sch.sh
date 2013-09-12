161537bfeedc814caf1b17f1f028c47ec7d65f2e
froyomuffin <froyomuffin@gmail.com>
2013-09-11 23:02:31 -0400

    Fixed room number case, added case variables, moved debug prints

--- sch.sh
+++ sch.sh
@@ -1,53 +1,53 @@
 #!/bin/bash
+
+#Input (CRN)
 CRN=$1
+
 SLICESIZE=100
+
 SLICE=$(cat 20130906.html | grep -A $SLICESIZE "=$CRN&")
 CRNLINES=$(echo "$SLICE" | grep -n "https://horizon.mcgill.ca/pban1/bwckschd.p_disp_listcrse?term_in=" | cut -d: -f1)
 NEXTCRNLINE=$(echo $CRNLINES | cut -d' ' -f2)
 TRIMMEDSLICE=$(echo "$SLICE" | sed ''${NEXTCRNLINE}',$d')
-#echo "$TRIMMEDSLICE"
 
 SUBJECTOUT=$(echo "$TRIMMEDSLICE" | grep -A 4 "https://horizon.mcgill.ca/pban1/bwckschd.p_disp_listcrse?term_in=" | sed -n '1!p' | sed 's/<[^>]\+>//g' | paste -sd " ")
 
 TITLETOUT=$(echo "$TRIMMEDSLICE" | sed -n '7p' | sed 's/<[^>]\+>//g')
 
-echo "$SUBJECTOUT"
-
-echo "$TITLETOUT"
-
 COURSESLOTS=$(echo "$TRIMMEDSLICE" | egrep -B1 -A7 " AM-| PM-" | sed 's/<[^>]\+>//g' | grep -v "\-\-")
-
-#echo "$COURSESLOTS"
-
-#SLOTS=$(echo "$TRIMMEDSLICE" | egrep -B 1 -A 7 " AM-| PM-" | sed 's/<[^>]\+>//g' | paste -sd " ")
 
 COUNTER=1
 while read p; do
 	MODULO=$(( $COUNTER % 9 ))
 	case $MODULO in
-		1)	echo $p
+		1)	#Day of week
+			DAYOFWEEK="$p"
+			echo "Processed day of week"
 			;;
-		2)	echo $p
+		2)	#Time slot
+			TIMESLOT="$p"
+			echo "Processed time slot"
 			;;
-		8)	echo $p
+		8)	#Date range
+			DATERANGE="$p"
+			echo "Processed date range"
 			;;
-		9)	echo $p
+		0)	#Room number
+			ROOMNUMBER="$p"
+			echo "Processed room number"
 			;;
-
 #		*)	echo "normal"
 #			;;
 	esac
 	COUNTER=$(( $COUNTER + 1 ))
-	#echo $p
 done <<< "$COURSESLOTS"
 
-#SLOTS=$(echo "$TRIMMEDSLICE" | egrep -B 1 -A 7 " AM-| PM-" | sed 's/<[^>]\+>//g' | paste -sd " ")
-
-#NAMEOUT=$(echo "$TRIMMEDSLICE" | grep -A 4 "https://horizon.mcgill.ca/pban1/bwckschd.p_disp_listcrse?term_in=" | sed -n '1!p' | sed 's/<[^>]\+>//g' | paste -sd " ")
-
-#DATEOUT=$(echo "$TRIMMEDSLICE" | egrep -B 2 ">Active<" | sed 's/<[^>]\+>//g' | paste -sd " ")
 
 
-#echo "$NAMEOUT"
-#echo "$SLOTOUT" 
-#echo "$DATEOUT"
+#===== Some debug prints =====
+#Course code
+#echo "$SUBJECTOUT"
+
+#Course title
+#echo "$TITLETOUT"
+
